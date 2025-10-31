1cd6887a8ef3ed8b82e499844574490796d1a37f
unknown <amirhosein279279@gmail.com>
2025-10-31 19:44:16 -0400

    Add Render deployment configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,28 +5,49 @@ plugins {
     kotlin("jvm") version "2.2.20"
     id("io.ktor.plugin") version "3.3.1"
     kotlin("plugin.serialization") version "2.2.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.example"
 version = "0.0.1"
 
 application {
-    mainClass = "io.ktor.server.netty.EngineMain"
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-config-yaml")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-status-pages")
-    implementation("io.ktor:ktor-server-html-builder")
+    
     implementation("org.apache.commons:commons-compress:1.24.0")
+    implementation("net.lingala.zip4j:zip4j:2.11.5")
     implementation("com.github.junrar:junrar:7.5.5")
-    implementation("net.lingala.zip4j:zip4j:2.11.5") // For password-protected ZIP
-    testImplementation("io.ktor:ktor-server-test-host")
+    
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks {
+    shadowJar {
+        archiveBaseName.set("file-compression-tool")
+        archiveClassifier.set("all")
+        archiveVersion.set("")
+        mergeServiceFiles()
+        manifest {
+            attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+        }
+    }
+    
+    build {
+        dependsOn(shadowJar)
+    }
 }
